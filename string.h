11431f2a57da75b5a00fbc4f7b6dfc31d3191ed6
TokyoOwl <121123083+TokyoOwl@users.noreply.github.com>
2024-04-23 15:07:37 +0300

    Update string.h

--- string.h
+++ string.h
@@ -17,7 +17,7 @@ public:
 	void toUpper();
 	void toLower();
 	String(const String &c_str); //конструктор копирования
-	String& operator=(const String &c_str);//оператор присваивания
+	operator=(const String c_str);//оператор присваивания
 	void print_data(int data);
 	void print_data(double data);
 	void print_data(string data);
