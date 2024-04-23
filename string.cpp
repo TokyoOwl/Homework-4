179037fb3633a2e5efe500eb9930a70c34081ddf
TokyoOwl <121123083+TokyoOwl@users.noreply.github.com>
2024-04-23 15:09:17 +0300

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -76,7 +76,7 @@ String::String concat(const String& otherString)
 			str[i] = c_str.str[i];
 		str[size] = '\0';
 	}
-	String::operator=(const String c_str) //оператор присваивания
+	String& String::operator=(const String &c_str) //оператор присваивания
 	{
 		size = c_str.size;
 		str = new char[size + 1];
