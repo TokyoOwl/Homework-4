d5d77f8a928885bd150af35648b12b9c35d0379e
TokyoOwl <121123083+TokyoOwl@users.noreply.github.com>
2024-04-23 14:56:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "person.h"
+#include "shape.h"
 using namespace std;
 int main()
 {
-  Person Mike("Mike", "Vazovsky", 29);
+	Circle a(5);
+	a.print_area();
+  	Rectangle b(2, 4);
+  	b.print_area();
+  	Triangle c(4, 10);
+  	c.print_area();
+  	Person Mike("Mike", "Vazovsky", 29);
 	Mike.display_info(1);
 	Mike.display_info(1, 2);
 	Mike.display_info(1, 2, 3);
