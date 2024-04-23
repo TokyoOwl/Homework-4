78525663eafeabf4b22552f4302df0494356cc86
TokyoOwl <121123083+TokyoOwl@users.noreply.github.com>
2024-04-23 14:54:22 +0300

    Update shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -1,74 +1,42 @@
-#include <iostream>
-using namespace std;
-class Shape
+virtual double Shape::calculate_area(double base1, double base2, double height)
 {
-public:
-  virtual double calculate_area(double base1, double base2, double height)
-  {
-    return ((base1 + base2) / 2 ) * height;
-  }
-};
-class Circle : Shape 
+  return ((base1 + base2) / 2 ) * height;
+}
+Circle::Circle(double radius)
 {
-  double radius;
-public:
-  Circle(double radius){
-    this->radius = radius;
-  }
-  double calculate_area(double radius)
-  {
-    return 3.14*radius*radius;
-  }
-  void print_area()
-  {
-    cout << calculate_area(radius) << endl;
-  }
-};
-class Rectangle : Shape 
+  this->radius = radius;
+}
+double Circle::calculate_area(double radius)
 {
-  double length;
-  double width;
-public:
-  Rectangle(double length, double width)
-  {
-    this->length = length;
-    this->width = width;
-  }
-  double calculate_area(double length, double width)
-  {
-    return length * width;
-  }
-  void print_area()
-  {
-    cout << calculate_area(length, width) << endl;
-  }
-};
-class Triangle : Shape 
+  return 3.14*radius*radius;
+}
+void Circle::print_area()
 {
-  int base;
-  int height;
-public:
-  Triangle(int base, int height)
-  {
-    this->base = base;
-    this->height = height;
-  }
-  double calculate_area(int base, int height)
-  {
-    return 0.5*base*height;
-  }
-  void print_area()
-  {
-    cout << calculate_area(base, height) << endl;
-  }
-};
-int main()
+  cout << calculate_area(radius) << endl;
+}
+Rectangle::Rectangle(double length, double width)
 {
-  Circle a(5);
-  a.print_area();
-  Rectangle b(2, 4);
-  b.print_area();
-  Triangle c(4, 10);
-  c.print_area();
-  return 0;
+  this->length = length;
+  this->width = width;
+}
+double Rectangle::calculate_area(double length, double width)
+{
+  return length * width;
+}
+void Rectangle::print_area()
+{
+  cout << calculate_area(length, width) << endl;
+}
+Triangle::Triangle(int base, int height)
+{
+  this->base = base;
+  this->height = height;
+}
+double Triangle::calculate_area(int base, int height)
+{
+  return 0.5*base*height;
+}
+void Triangle::print_area()
+{
+  cout << calculate_area(base, height) << endl;
 }
